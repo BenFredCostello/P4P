d6c5518de942898c32434739a9ad672633a6102c
tpur744 <tpur744@aucklanduni.ac.nz>
2026-07-08 01:10:44 +1200

    phone app initial

---
+++ TranscriberGlasses/TranscriberGlasses/TranscriberGlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriberGlassesApp.swift
+//  TranscriberGlasses
+//
+//  Created by Tavish Puri on 08/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TranscriberGlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
